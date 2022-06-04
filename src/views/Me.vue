15e65d0ea4d4733d6d9e24b608ce8ad49fd45631
Glenn <glenou@yahoo.com>
2022-06-05 03:13:09 +0800

    icon filllin

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -32,19 +32,94 @@
 
       <div class="order-area">
         我的订单
-        <span v-show="!showOrderAll" @click="chekcAllOrder">查看全部</span>
+        <span class="showAll" @click="chekcAllOrder"
+          >查看全部 <van-icon name="arrow"
+        /></span>
         <van-divider />
+
+        <van-row type="flex" justify="center">
+          <van-col span="5">
+            <van-icon name="shop" color="red"> </van-icon>
+            <span class="text">代付款</span></van-col
+          >
+          <van-col span="5">
+            <van-icon name="shop-collect" color="red"> </van-icon>
+            <span class="text">代发货</span>
+          </van-col>
+          <van-col span="5">
+            <van-icon name="clock" color="red"> </van-icon>
+            <span class="text">代收款</span></van-col
+          >
+          <van-col span="5">
+            <van-icon name="alipay" color="red"> </van-icon>
+            <span class="text">退款退货</span></van-col
+          >
+        </van-row>
       </div>
       <!-- sercvice area -->
       <div class="service-area">
         我的服务
-        <span>查看全部</span>
+        <span class="showAll">查看全部 <van-icon name="arrow" /> </span>
         <van-divider />
-        <span>邀请好友</span>
+
+        <van-row type="flex" justify="center">
+          <van-col span="5">
+            <van-icon name="youzan-shield" color="red"> </van-icon>
+            <span class="text">邀请好友</span></van-col
+          >
+          <van-col span="5">
+            <van-icon name="bell" color="red"> </van-icon>
+            <span class="text">签到赢礼包</span>
+          </van-col>
+          <van-col span="5">
+            <van-icon name="coupon" color="red"> </van-icon>
+            <span class="text">优惠券</span></van-col
+          >
+          <van-col span="5">
+            <van-icon name="audio" color="red"> </van-icon>
+            <span class="text">限时购</span></van-col
+          >
+        </van-row>
         <van-divider />
-        <span>我的收藏</span>
+
+        <van-row type="flex" justify="center">
+          <van-col span="5">
+            <van-icon name="comment" color="red"> </van-icon>
+            <span class="text">我的收藏</span></van-col
+          >
+          <van-col span="5">
+            <van-icon name="gift" color="red"> </van-icon>
+            <span class="text">收货地址</span>
+          </van-col>
+          <van-col span="5">
+            <van-icon name="question" color="red"> </van-icon>
+            <span class="text">客户中心</span></van-col
+          >
+          <van-col span="5">
+            <van-icon name="todo-list" color="red"> </van-icon>
+            <span class="text">充值中心</span></van-col
+          >
+        </van-row>
         <van-divider />
-        <span>我的问答</span>
+
+        <van-row type="flex" justify="center">
+          <van-col span="5">
+            <van-icon name="manager" color="red"> </van-icon>
+            <span class="text">我的问答</span></van-col
+          >
+          <van-col span="5">
+            <van-icon name="label" color="red"> </van-icon>
+            <span class="text">领券中心</span>
+          </van-col>
+          <van-col span="5">
+            <van-icon name="service" color="red"> </van-icon>
+            <span class="text">验光单</span></van-col
+          >
+          <van-col span="5">
+            <van-icon name="chat" color="red"> </van-icon>
+            <span class="text">试用中心</span></van-col
+          >
+        </van-row>
       </div>
       <!-- log out button -->
       <van-button class="logou" type="default" block>退出登录</van-button>
@@ -56,7 +131,16 @@
 
 <script>
 import OrderAll from "../components/OrderAll.vue";
-import { Button, Divider, Image as VanImage } from "vant";
+import {
+  Button,
+  Divider,
+  Image as VanImage,
+  Icon,
+  Grid,
+  GridItem,
+  Col,
+  Row,
+} from "vant";
 export default {
   name: "me",
 
@@ -65,6 +149,11 @@ export default {
     [Button.name]: Button,
     [Divider.name]: Divider,
     [VanImage.name]: VanImage,
+    [Icon.name]: Icon,
+    [Grid.name]: Grid,
+    [GridItem.name]: GridItem,
+    [Col.name]: Col,
+    [Row.name]: Row,
   },
 
   data() {
@@ -97,6 +186,9 @@ export default {
 </script>
 
 <style>
+.me {
+  height: 720px;
+}
 .van-nav-bar {
   height: 2.8125rem;
   background: red;
@@ -133,12 +225,11 @@ export default {
   margin-left: 10px;
 }
 .name {
-
   margin-left: 10px;
 }
-.data{
+.data {
   padding-top: 10px;
-   margin-left: 27px;
+  margin-left: 27px;
 }
 .order-area {
   width: 100%;
@@ -149,8 +240,27 @@ export default {
 }
 .service-area {
   width: 100%;
-  height: 200px;
   background-color: white;
   margin-bottom: 10px;
+}
+.showAll {
+  float: right;
+}
+.van-icon {
+  color: red;
+  /* margin: 16px 0 16px; */
+  /* color: #323233; */
+  font-size: 30px;
+  margin-left: 5px;
+}
+.text {
+  display: block;
+  height: 36px;
+  /* margin: -4px 0 4px; */
+  padding: 0 5px;
+  color: #646566;
+  font-size: 12px;
+  line-height: 18px;
+  margin-top: 5px;
 }
 </style>
